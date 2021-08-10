cff29a1719cdc14dde02f1a2440b83e234d37280
jaqx <59521263+jaqx@users.noreply.github.com>
2021-08-09 21:05:55 -0700

    Final Project
    This includes all of my microservices and my teammates' microservice

--- project.js
+++ project.js
@@ -10,7 +10,14 @@ app.use(express.static('public'));
 app.use(express.json());
 app.set('port', 2022);
 
-//dictionary.com API key
+
+//wikipedia web page scraper
+const cheerio = require("cheerio");
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+
+
+//gets dictionary API key
 function keyFetch(){
   try {
     //get key and block execution until key is returned.
@@ -20,10 +27,45 @@ function keyFetch(){
   }
 }
 
-app.get('/', function(req,res){
-	res.render('home');
-});
+//create string request
+function dictRequest(word){
+  return "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" 
+         + word 
+         + "?key=" 
+         + keyFetch();
+}
 
+function cryptoRequest(buyer){
+  return "buyer=" 
+         + buyer
+         + "&jaq=0xc6EfbA1f45dD02294e4503F3dEFC6008cd7326dc";
+}
+
+//replaces all spaces in the string of the title with an underscore to create URL
+function makeUnderscore(title){
+  var splitString = title.split(' ')
+
+  let index = 0
+  var newString=''
+  while(splitString[index] != null)
+  {
+    newString += splitString[index]
+    //prevents putting underscore at end of string 
+    if(splitString[index+1] != null)
+      newString += '_'
+    index++
+  }
+  return newString;
+}
+
+let axiosConfig = {
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      "Access-Control-Allow-Origin": "*",
+  }
+};
+
+//dictionary API call 
 app.get('/dictionary', function(req,res){
 	for (var p in req.query){
 		axios.get(dictRequest(p))
@@ -36,21 +78,49 @@ app.get('/dictionary', function(req,res){
 	}
 });
 
-//create string request
-function dictRequest(word){
-  return "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" 
-         + word 
-         + "?key=" 
-         + keyFetch();
-}
+//gets summaries from wikipedia pages
+app.get('/wikime', function(req,res){
+	for (var title in req.query){
+		var url = "https://en.wikipedia.org/wiki/" + makeUnderscore(title);
+    axios.get(url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      res.send ($('div.mw-parser-output > p').text());
+    })
+    .catch(error => {
+      console.log(error);
+    });
+	}
+});
 
+//uses cryptocurrency to pay for tutoring
+app.get('/crypto', function(req,res){
+  for(var buyerSelect in req.query){
+    axios.post(
+      'http://ec2-52-78-121-194.ap-northeast-2.compute.amazonaws.com:31338/dApp/jaq',
+      cryptoRequest(buyerSelect),
+      axiosConfig
+    ).then((response) => {
+      res.send( response.data )
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+});
+
+//creating homepage
+app.get('/', function(req,res){
+	res.render('home');
+});
+
+//error page 
 app.use(function(req,res){
   res.render('404');
 });
 
+//start server
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
-
-
-
